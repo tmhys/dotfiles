321ca05e99fcff7e67902c5a236ab330748006eb
tmhys <t.h.tomy@gmail.com>
2020-04-11 13:40:04 +0900

    update

--- mklink.bat
+++
@@ -1,7 +0,0 @@
-REM Last Change: 29-Feb-2020.
-REM Maintainer:  TH
-REM シンボリックリンク作成
-mklink _vimrc  C:\Users\thtom\dotfiles\vimrc2\_vimrc
-mklink _gvimrc C:\Users\thtom\dotfiles\vimrc2\_gvimrc
-mklink _config C:\Users\thtom\dotfiles\vimrc2\_config
-mklink _gconfig C:\Users\thtom\dotfiles\vimrc2\_gconfig
