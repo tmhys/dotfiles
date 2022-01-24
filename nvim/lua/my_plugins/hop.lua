9fec9a8ddfeb8cb12a56abfce84164ed6dcd09ce
tmhys <t.h.tomy@gmail.com>
2022-01-24 22:58:42 +0900

    hop!

--- nvim/lua/my_plugins/hop.lua
+++ nvim/lua/my_plugins/hop.lua
@@ -3,8 +3,13 @@ require('hop').setup({
   keys = 'etovxqpdygfblzhckisuran'
 })
 
-vim.api.nvim_set_keymap('n', '<M-f>', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
-vim.api.nvim_set_keymap('n', '<M-F>', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
+require'hop'.setup {}
+
+--vim.api.nvim_set_keymap('n', 'SS', "<cmd>lua vim.cmd('DisableWhitespace'); require'hop'.hint_words()<CR>", {})
+--vim.api.nvim_set_keymap('x', 'SS', "<cmd>lua vim.cmd('DisableWhitespace'); require'hop'.hint_words()<CR>", {})
+
+vim.api.nvim_set_keymap('n', 'zz', "<cmd>lua require'hop'.hint_words()<CR>", {})
+--vim.api.nvim_set_keymap('n', '<M-F>', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
 --vim.api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
 --vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
 --vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
