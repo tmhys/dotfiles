d60310305cd6f1208a8566b638b5367c25bc5401
tmhys <t.h.tomy@gmail.com>
2022-08-10 00:03:43 +0900

    aaaa;wq: a

--- nvim/lua/my_plugins/lualine.lua
+++ nvim/lua/my_plugins/lualine.lua
@@ -10,33 +10,22 @@ local sections_1 = {
 	lualine_a = { "mode" },
 	lualine_b = { { "filetype", icon_only = true }, { "filename" } },
 	lualine_c = { { 'require("nvim-gps").get_location()', cond = is_available_gps } },
-    --lualine_c = { { gps.get_location, cond = gps.is_available } },
-	lualine_x = { "require'lsp-status'.status()", "diagnostics" ,
--- lualine_x = { "diagnostics" ,
---         sources = {'nvim_diagnostic'},
---    symbols = {
---  error = "E",
---  hint = "H",
---  info = "I",
---  warn = "W"
--- },
-        --symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}
-	--lualine_x = {
-    --    "diagnostics",
-    --sources = {'nvim_lsp'},
-    --sections = {'error', 'warn', 'info', 'hint'},
-    --diagnostics_color = {
-    --        -- Same values as the general color option can be used here.
-    --        error = 'DiagnosticError', -- Changes diagnostics' error color.
-    --        warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
-    --        info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
-    --        hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
-    --      },
-    --      symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
-    --      colored = true,           -- Displays diagnostics status in color if set to true.
-    --      update_in_insert = false, -- Update diagnostics in insert mode.
-    --      always_visible = false,   -- Show diagnostics even if there are none.
-    },
+	-- lualine_x = { "diagnostics"  },
+    lualine_x = {
+    {
+      'diagnostics',
+
+      -- Table of diagnostic sources, available sources are:
+      --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+      -- or a function that returns a table as such:
+      --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+      sources = { 'nvim_diagnostic'},
+
+
+      -- symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
+      symbols = {error = 'ðŸ…´ ', warn = 'ðŸ††"', info = 'ðŸ…¸ ', hint = 'ðŸ…· '},
+    }
+  },
 	lualine_y = { "branch", "diff", "g:asyncrun_status" },
 	lualine_z = { "location" },
 }
@@ -66,106 +55,6 @@ end
 
 vim.api.nvim_set_keymap("n", "!", "<Cmd>lua LualineToggle()<CR>", { noremap = true, silent = true })
 
---local colors = {
---	-- onedark
---	-- blue = '#61afef',
---	-- green = '#98c379',
---	-- purple = '#c678dd',
---	-- red1 = '#e06c75',
---	-- red2 = '#be5046',
---	-- yellow = '#e5c07b',
---	-- fg = '#abb2bf',
---	-- bg = '#282c34',
---	-- gray1 = '#5c6370',
---	-- gray2 = '#2c323d',
---	-- gray3 = '#3e4452'
---	-- nordfox
---	black = "#3b4252",
---	red = "#bf616a",
---	green = "#a3be8c",
---	yellow = "#ebcb8b",
---	blue = "#81a1c1",
---	magenta = "#b48ead",
---	cyan = "#88c0d0",
---	white = "#e5e9f0",
---	foreground = "#b9bfca",
---	background = "#2e3440",
---}
---
---local terminal_status_color = function(status)
---	local mode_colors = {
---		Running = colors.orange,
---		Finished = colors.purple,
---		Success = colors.blue,
---		Error = colors.red,
---		Command = colors.green,
---	}
---
---	return mode_colors[status]
---end
-
---local get_exit_status = function()
---	local ln = vim.api.nvim_buf_line_count(0)
---	while ln >= 1 do
---		local l = vim.api.nvim_buf_get_lines(0, ln - 1, ln, true)[1]
---		ln = ln - 1
---		local exit_code = string.match(l, "^%[Process exited ([0-9]+)%]$")
---		if exit_code ~= nil then
---			return tonumber(exit_code)
---		end
---	end
---end
-
---local terminal_status = function()
---	if
---		vim.api.nvim_exec(
---			[[echo trim(execute("filter /" . escape(nvim_buf_get_name(bufnr()), '~/') . "/ ls! uaF"))]],
---			true
---		) ~= ""
---	then
---		local result = get_exit_status()
---		if result == nil then
---			return "Finished"
---		elseif result == 0 then
---			return "Success"
---		elseif result >= 1 then
---			return "Error"
---		end
---		return "Finished"
---	end
---	if
---		vim.api.nvim_exec(
---			[[echo trim(execute("filter /" . escape(nvim_buf_get_name(bufnr()), '~/') . "/ ls! uaR"))]],
---			true
---		) ~= ""
---	then
---		return "Running"
---	end
---	return "Command"
---end
---
---local function get_terminal_status()
---	if vim.bo.buftype ~= "terminal" then
---		return ""
---	end
---	local status = terminal_status()
---	vim.api.nvim_command(
---		"hi LualineToggleTermStatus guifg=" .. colors.background .. " guibg=" .. terminal_status_color(status)
---	)
---	return status
---end
---
---local function toggleterm_statusline()
---	return "ToggleTerm #" .. vim.b.toggle_number
---end
---
---local my_toggleterm = {
---	sections = {
---		lualine_a = { toggleterm_statusline },
---		lualine_z = { { get_terminal_status, color = "LualineToggleTermStatus" } },
---	},
---	filetypes = { "toggleterm" },
---}
 
 local my_extension = {
 	sections = { lualine_b = { "filetype" } },
@@ -177,10 +66,10 @@ require("lualine").setup({
 		icons_enabled = true,
 		--theme = "nightfox",
 		theme = "auto",
-        --component_separators = { left = 'ðò', right = 'ðô'},
-        --section_separators = { left = 'ðñ', right = 'ðó'},
-        --component_separators = { left = 'ûž±', right = 'ûž³'},
-        --section_separators = { left = 'ûž°', right = 'ûž²'},
+        --component_separators = { left = 'î‚±', right = 'î‚³'},
+        --section_separators = { left = 'î‚°', right = 'î‚²'},
+        --component_separators = { left = 'è–°ï½±', right = 'è–°ï½³'},
+        --section_separators = { left = 'è–°ï½°', right = 'è–°ï½²'},
 		----component_separators = { left = "\ue0b0", right = "\ue0b2" },
 		----section_separators = { left = "\ue0b1", right = "\ue0b3" },
 		--component_separators = { left = "", right = "" },
@@ -201,35 +90,3 @@ require("lualine").setup({
 	--extensions = { "quickfix", my_toggleterm, "symbols-outline", my_extension },
 	extensions = { "quickfix", my_toggleterm, "symbols-outline", my_extension },
 })
-
---require("lualine").setup({
---	options = {
---		icons_enabled = true,
---		--theme = "nightfox",
---		theme = "auto",
---		component_separators = { left = "", right = "" },
---		section_separators = { left = "", right = "" },
---		disabled_filetypes = {},
---		always_divide_middle = true,
---	},
---	sections = {
---	lualine_a = { "mode" },
---	lualine_b = { { "filetype", icon_only = true }, { "filename" } },
---	--lualine_c = { { 'require("nvim-gps").get_location()', cond = is_available_gps } },
---    lualine_c = { { gps.get_location, cond = gps.is_available } },
---	lualine_x = { "require'lsp-status'.status()", "diagnostics" },
---	lualine_y = { "branch", "diff", "g:asyncrun_status" },
---	lualine_z = { "location" },
---    },
---	inactive_sections = {
---		lualine_a = { "mode" },
---		lualine_b = {},
---		lualine_c = { "filename" },
---		lualine_x = { "location" },
---		lualine_y = {},
---		lualine_z = {},
---	},
---	tabline = {},
---	--extensions = { "quickfix", my_toggleterm, "symbols-outline", my_extension },
---	extensions = { "quickfix", my_toggleterm, "symbols-outline", my_extension },
---})
