48f0c061a9b7334cdd31c75cd3dbc12ab7318cac
tmhys <t.h.tomy@gmail.com>
2023-05-08 22:29:49 +0900

    null-ls

--- nvim/lua/my_plugins/mason_tool_installer.lua
+++ nvim/lua/my_plugins/mason_tool_installer.lua
@@ -1,27 +1,27 @@
+require("mason-tool-installer").setup({
 
-require('mason-tool-installer').setup {
+	-- a list of all tools you want to ensure are installed upon
+	-- start; they should be the names Mason uses for each tool
+	ensure_installed = {
 
-  -- a list of all tools you want to ensure are installed upon
-  -- start; they should be the names Mason uses for each tool
-  ensure_installed = {
+		-- you can pin a tool to a particular version
+		-- { 'golangci-lint', version = 'v1.47.0' },
 
-    -- you can pin a tool to a particular version
-    -- { 'golangci-lint', version = 'v1.47.0' },
+		-- you can turn off/on auto_update per tool
+		-- { 'bash-language-server', auto_update = true },
 
-    -- you can turn off/on auto_update per tool
-    -- { 'bash-language-server', auto_update = true },
-
-    'stylua',
-    'lua-language-server',
-    'vim-language-server',
-    'autopep8',
-    'isort',
-    'black',
-    'pyright',
-    -- 'python-lsp-server',
-    -- 'gopls',
-    -- 'vimls',
-    --[[ 'gofumpt',
+		"stylua",
+		"lua-language-server",
+		"vim-language-server",
+		"autopep8",
+		"isort",
+		"black",
+		"pyright",
+		"prettier",
+		-- 'python-lsp-server',
+		-- 'gopls',
+		-- 'vimls',
+		--[[ 'gofumpt',
     'golines',
     'gomodifytags',
     'gotests',
@@ -34,32 +34,32 @@ require('mason-tool-installer').setup {
     'shfmt',
     'staticcheck',
     'vint', ]]
-  },
+	},
 
-  -- if set to true this will check each tool for updates. If updates
-  -- are available the tool will be updated. This setting does not
-  -- affect :MasonToolsUpdate or :MasonToolsInstall.
-  -- Default: false
-  auto_update = true,
+	-- if set to true this will check each tool for updates. If updates
+	-- are available the tool will be updated. This setting does not
+	-- affect :MasonToolsUpdate or :MasonToolsInstall.
+	-- Default: false
+	auto_update = true,
 
-  -- automatically install / update on startup. If set to false nothing
-  -- will happen on startup. You can use :MasonToolsInstall or
-  -- :MasonToolsUpdate to install tools and check for updates.
-  -- Default: true
-  run_on_start = true,
+	-- automatically install / update on startup. If set to false nothing
+	-- will happen on startup. You can use :MasonToolsInstall or
+	-- :MasonToolsUpdate to install tools and check for updates.
+	-- Default: true
+	run_on_start = true,
 
-  -- set a delay (in ms) before the installation starts. This is only
-  -- effective if run_on_start is set to true.
-  -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
-  -- Default: 0 start_delay = 3000, -- 3 second delay
-  start_delay = 3000, -- 3 second delay
-}
+	-- set a delay (in ms) before the installation starts. This is only
+	-- effective if run_on_start is set to true.
+	-- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+	-- Default: 0 start_delay = 3000, -- 3 second delay
+	start_delay = 3000, -- 3 second delay
+})
 
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'MasonToolsUpdateCompleted',
-    callback = function()
-      vim.schedule(function()
-        print 'mason-tool-installer has finished'
-      end)
-    end,
-  })
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MasonToolsUpdateCompleted",
+	callback = function()
+		vim.schedule(function()
+			print("mason-tool-installer has finished")
+		end)
+	end,
+})
