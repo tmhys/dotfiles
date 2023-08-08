1cd0e51713eea1326904cd1cbf247014af0048d9
tmhys <t.h.tomy@gmail.com>
2023-08-08 22:43:51 +0900

    update

--- nvim/lua/my_plugins/treesitter.lua
+++ nvim/lua/my_plugins/treesitter.lua
@@ -35,15 +35,15 @@ require("nvim-treesitter.configs").setup({
 	ensure_installed = "all",
 })
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.seq = {
-	install_info = {
-		url = "~/vimfiles/tree-sitter-seq", -- local path or git repo
-		files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-		-- optional entries:
-		branch = "main", -- default branch in case of git repo if different from master
-		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-	},
-	filetype = "seq", -- if filetype does not match the parser name
-}
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser_config.seq = {
+--  install_info = {
+--      url = "~/vimfiles/tree-sitter-seq", -- local path or git repo
+--      files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+--      -- optional entries:
+--      branch = "main", -- default branch in case of git repo if different from master
+--      generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+--      requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+--  },
+--  filetype = "seq", -- if filetype does not match the parser name
+-- }
